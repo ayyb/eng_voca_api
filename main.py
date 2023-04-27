d42e37d415cd784317f9fed3b45b016051198e68
ayyb <ayyb@kakao.com>
2023-04-27 18:27:55 +0900

    main example write

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sqlite3
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# uvicorn main:app --reload 서버 시동
+# sqllit3 에서 테이블이랑 db 조회해서 확인해보기
+
 conn = sqlite3.connect('test.db')
 c = conn.cursor()
 app = FastAPI()
@@ -22,5 +25,17 @@ app.add_middleware(
 
 
 @app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/createMember")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/login")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/user")
 async def root():
     return {"message": "Hello World"}
